9a308b59049de2f93f7447a6e23c1a56de3387aa
Juanca <riberojuanca@gmail.com>
2024-07-29 19:55:56 -0300

    Carousel color changed

--- src/components/CarouselHeader.jsx
+++ src/components/CarouselHeader.jsx
@@ -46,7 +46,7 @@ const CarouselHeader = () => {
   return (
     <section className="mt-24">
       <Carousel
-        className="sm:w-full p-4 sm:p-0 sm:bg-black "
+        className="sm:w-full p-4 sm:p-0 sm:bg-white "
         plugins={[
           Autoplay({
             delay: 4000,
@@ -58,13 +58,13 @@ const CarouselHeader = () => {
             <CarouselItem
               key={id}
               onClick={() => router.push(link)}
-              className="lg:w-screen cursor-pointer"
+              className="lg:w-screen"
             >
               <div className="sm:rounded-lg ">
-                <Card className=" flex items-center justify-center sm:w-fit sm:h-80 sm:mt-0 bg-black  text-white shadow-none border-none">
+                {/* <Card className=" flex items-center justify-center sm:w-fit sm:h-80 sm:mt-0 bg-white  text-black shadow-none border-none">
                   <CardContent className="flex sm:flex-row flex-col justify-center gap-6 items-center text-center ">
                     <img
-                      className="rounded-md sm:rounded-xl h-80 sm:h-60 mt-6 sm:pt-0 sm:mt-6 sm:w-2/5 object-cover"
+                      className="rounded-md h-80 sm:h-72 mt-6 sm:pt-0 sm:mt-6 sm:w-2/5 object-cover "
                       src={image}
                       width={640}
                       height={640}
@@ -76,6 +76,17 @@ const CarouselHeader = () => {
                         {description}
                       </p>
                     </div>
+                  </CardContent>
+                </Card> */}
+                <Card className=" flex items-center justify-center sm:w-screen sm:h-80 sm:mt-0 bg-white  text-black shadow-none border-none">
+                  <CardContent className="flex sm:flex-row flex-col justify-center items-center text-center w-screen">
+                    <img
+                      className="cursor-pointer rounded-md h-80 sm:h-72 mt-6 sm:pt-0 sm:mt-6 sm:w-3/4 object-cover grayscale "
+                      src={image}
+                      width={640}
+                      height={640}
+                      alt=""
+                    ></img>
                   </CardContent>
                 </Card>
               </div>
